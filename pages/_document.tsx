2eab515f07795b25b0f10eaa29ac63996fa3698b
Mirco Kraenz <kraenzmirco@gmail.com>
2020-09-27 16:59:07 +0200

    ADD #1 favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,11 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" /> 
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
         </Head>
         <body>
           <Main />
