f1d657755847d0399b836ff9bce6ff17f1a75b16
Mirco Kraenz <kraenzmirco@gmail.com>
2020-09-27 20:13:38 +0200

    REMOVE #1 leftover boilerplate

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,15 +1,9 @@
-import { isProductionBrowser } from "../isProductionBrowser";
-
 export const CONFIG = {
-    levelsMetadataUri: isProductionBrowser
-        ? "https://r7qyjnm5u0.execute-api.eu-central-1.amazonaws.com/prod/prod/levels"
-        : "http://localhost:3140/prod/levels",
     emailJs: {
         serviceId: "default_service",
         templateId: "template_QsOH5koE",
         userId: "user_BsXgSjnZ05nGtNjMvebDF",
     },
-    gameUrl: "https://prosingularity.github.io/fursorger/",
     googleAnalyticsId: "FAKE ID", // TODO create a new app
 };
 
