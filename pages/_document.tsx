97752b1b766f079575a8796cc313d524501633e1
Mirco Kraenz <kraenzmirco@gmail.com>
2020-10-19 20:56:47 +0200

    FIX #1 unnecessary import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import { ServerStyleSheets } from "@material-ui/core/styles";
 import Document, { Head, Main, NextScript } from "next/document";
 import React from "react";
-import { content } from "../src/content";
 import theme from "../src/theme";
 
 export default class MyDocument extends Document {
