866876b68b91032a1fe25790965188eeeec6d322
Mirco Kraenz <software@kraenz.eu>
2025-03-14 09:59:18 +0900

    fix: duplicate entry in techs

--- src/content.ts
+++ src/content.ts
@@ -153,7 +153,6 @@ export const content = {
         "Sentry",
         "SocketIO & Websockets",
         "LauchDarkly",
-        "Lokalise",
         "Heroku",
         "Platform.sh",
         "Nylas",
